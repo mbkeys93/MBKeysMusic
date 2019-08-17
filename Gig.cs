6cd3d686e2f10e379eda0bda250ce09d5d3c7c5f
Marcus Bolden <marcus.bolden@hotmail.com>
2019-08-17 17:25:53 -0400

    some models and razor pages from this project

---
+++ Gig.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBKeysMusic.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal Payment { get; set; }
+
+        [Display(Name = "Performance Date")]
+        [DataType(DataType.Date)]
+        public DateTime PerformanceDate { get; set; }
+
+        [Range(1, 10)]
+        [Required]
+        public int Practices { get; set; }
+    }
+}
---
+++ SeedData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MBKeysMusic.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MBKeysMusicContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MBKeysMusicContext>>()))
+            {
+                // Look for any movies.
+                if (context.Gig.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Gig.AddRange(
+                    new Gig
+                    { 
+                        Name = "GSU Recital",
+                        Description = "Playing for my friend's recital for school.",
+                        Payment = 0M,
+                        PerformanceDate = DateTime.Parse("2019-2-21"),
+                        Practices = 3                
+                    },
+
+                    new Gig
+                    {
+                        Name = "Youth Service",
+                        Description = "Playing for teen/children's church every week.",
+                        Payment = 200M,
+                        PerformanceDate = DateTime.Parse("2019-2-17"),
+                        Practices = 1,
+                    },
+
+                    new Gig
+                    {
+                        Name = "Wedding",
+                        Description = "Playing for a wedding.",
+                        Payment = 200M,
+                        PerformanceDate = DateTime.Parse("2019-3-21"),
+                        Practices = 3,
+                    },
+
+                    new Gig
+                    {
+                        Name = "Club Gig",
+                        Description = "Playing at a club/bar.",
+                        Payment = 200M,
+                        PerformanceDate = DateTime.Parse("2019-4-21"),
+                        Practices = 2,
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
